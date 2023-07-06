fc1c944f3ae09bc4ef499e8da0906cea362b1f80
Rachel <rachelbrockman1@gmail.com>
2023-07-06 19:44:51 -0400

    add report function

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    chars_dict = count_letters(text)
+    print(chars_dict)
+    report = print_report(chars_dict)
+    print(f"{report}")
+
+
 def count_words(words):
     word = words.split()
     number_words = len(word)
     print(f"Number of words in book: {number_words}")
 
 
-def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        word_count = count_words(file_contents)
+def count_letters(text):
+    char_count = {}  # empty dictionary
+    for character in text:
+        lower_case = character.lower()
+        if lower_case.isalpha():
+            if lower_case in char_count:
+                char_count[lower_case] += 1
+            else:
+                char_count[lower_case] = 1
+    return char_count
+
+
+def print_report(char_count):
+    report = []
+    for character, count in char_count.items():
+        report.append((f"The {character}", f" character was found {count} times"))
+    report.sort()
+    return report
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 main()
